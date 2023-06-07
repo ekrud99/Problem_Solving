6e66c2636bbb4ca0016596c707253160168ca4c9
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-06-07 23:07:49 +0900

    [Gold II] Title: 트리의 지름, Time: 84 ms, Memory: 9732 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+
+#define endl "\n" // 줄바꿈
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i) 
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+
+#define pii pair<int, int>
+
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define PIV 1 << 20
+
+using namespace std; // std 클래스에 있는것을 사용하겠다
+typedef long long ll;
+typedef unsigned long long ull;
+
+int v, flag;
+vector<vector<pii>> tree;
+
+vector<int> dist, visited;
+
+void bfs(int node){
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
+
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        rep(i, 0, tree[cur].size()){
+            pii edge = tree[cur][i];
+            if(visited[edge.first]) continue;
+            visited[edge.first] = true;
+            q.push(edge.first);
+
+            dist[edge.first] = dist[cur] + edge.second;
+        }
+    }
+
+}
+
+int main(){
+    FAST; // 입출력 빠르게 하기 위해
+#ifndef ONLINE_JUDGE 
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+    cin >> v;
+    tree.resize(v + 1);
+    dist.resize(v + 1);
+    visited.resize(v + 1);
+    REP(i, 0, v){
+        int s;
+        cin >> s;
+
+        while(1){
+            int e, w;
+            cin >> e;
+            if(e == -1) break;
+            cin >> w;
+            tree[s].pb(make_pair(e, w));
+        }
+    }
+
+    bfs(1);
+    int maxIdx = 1;
+
+    REP(i, 2, v){
+        if(dist[maxIdx] < dist[i]) maxIdx = i;
+    }
+    fill(dist.begin(), dist.end(), 0);
+    fill(visited.begin(), visited.end(), 0);
+    bfs(maxIdx);
+
+    sort(dist.begin(), dist.end());
+
+    cout << dist[v];
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+    
+#endif
+
+    return 0;
+}
