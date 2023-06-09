fbd9f52a5ce7a458d5f593ec2eb5bc135e6128e0
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-06-09 17:30:53 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 24 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define endl "\n" // 줄바꿈
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+
+#define pii pair<int, int>
+
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define PIV 1 << 20
+
+using namespace std; // std 클래스에 있는것을 사용하겠다
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(){
+    FAST;
+#ifndef ONLINE_JUDGE
+    clock_t start = clock();
+    freopen("input.txt", "r", stdin);
+#endif
+
+    cin >> n;
+    rep(i, 0, n){
+        int card;
+        cin >> card;
+        pq.push(card);
+    }
+
+    int c1 = 0;
+    int c2 = 0;
+    int sum = 0;
+
+    while(pq.size() != 1){
+        c1 = pq.top();
+        pq.pop();
+        c2 = pq.top();
+        pq.pop();
+        sum += c1 + c2;
+        pq.push(c1 + c2);
+    }
+
+    cout << sum;
+
+
+#ifndef ONLINE_JUDGE
+    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
+    
+#endif
+
+    return 0;
+}
