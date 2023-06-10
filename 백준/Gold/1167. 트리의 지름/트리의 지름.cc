8110ddf321ca8813506e2d16b5d73d95f7a60db0
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-06-10 20:51:43 +0900

    [Gold II] Title: 트리의 지름, Time: 76 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -1,12 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <cstring>
+#include <bits/stdc++.h>
 
-
-#define endl "\n" // 줄바꿈
+#define endl "\n"
 #define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define rep(i, a, b) for(auto i = a; i < b; ++i) 
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
 #define REP(i, a, b) for(auto i = a; i <= b; ++i)
 
 #define pii pair<int, int>
@@ -16,46 +12,44 @@
 #define INF numeric_limits<int>::max()
 #define PIV 1 << 20
 
-using namespace std; // std 클래스에 있는것을 사용하겠다
+using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
-int v, flag;
-vector<vector<pii>> tree;
-int dist[100001], visited[100001];
 
-void bfs(int node){
-    queue<int> q;
-    q.push(node);
+//dfs 사용 풀이
+
+typedef pii branch;
+
+vector<vector<branch>> tree;
+int visited[100001], dist[100001];    
+
+void dfs(int node){
+    if(visited[node]) return;
     visited[node] = 1;
 
-    while(!q.empty()){
-        int cur = q.front();
-        q.pop();
-        rep(i, 0, tree[cur].size()){
-            pii edge = tree[cur][i];
-            if(visited[edge.first]) continue;
-            visited[edge.first] = true;
-            q.push(edge.first);
 
-            dist[edge.first] = dist[cur] + edge.second;
-        }
+    rep(i, 0, tree[node].size()){
+        pii cur_branch = tree[node][i];
+        if(visited[cur_branch.first]) continue;
+        dist[cur_branch.first] = cur_branch.second + dist[node];
+        dfs(cur_branch.first);
     }
-
 }
 
 int main(){
-    FAST; // 입출력 빠르게 하기 위해
-#ifndef ONLINE_JUDGE 
+    FAST;
+#ifndef ONLINE_JUDGE
     clock_t start = clock();
     freopen("input.txt", "r", stdin);
 #endif
+
+    int v;
     cin >> v;
     tree.resize(v + 1);
-    
-    REP(i, 0, v){
+
+    REP(i, 1, v){
         int s;
         cin >> s;
-
         while(1){
             int e, w;
             cin >> e;
@@ -65,29 +59,22 @@ int main(){
         }
     }
 
-    bfs(1);
-    int maxIdx = 1;
-
-    REP(i, 2, v){
-        if(dist[maxIdx] < dist[i]) maxIdx = i;
+    dfs(1);
+    int max_idx = 1;
+    REP(i, 0, v){
+        if(dist[max_idx] < dist[i]) max_idx = i;
     }
-    memset(dist, 0, sizeof(dist));
     memset(visited, 0, sizeof(visited));
-    bfs(maxIdx);
-
-    maxIdx = 1;
-
-    REP(i, 2, v){
-        if(dist[maxIdx] < dist[i]) maxIdx = i;
+    memset(dist, 0, sizeof(dist));
+    dfs(max_idx);
+    REP(i, 0, v){
+        if(dist[max_idx] < dist[i]) max_idx = i;
     }
 
-
-    cout << dist[maxIdx];
-
+    cout << dist[max_idx];
 #ifndef ONLINE_JUDGE
     cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
     
 #endif
-
     return 0;
 }
