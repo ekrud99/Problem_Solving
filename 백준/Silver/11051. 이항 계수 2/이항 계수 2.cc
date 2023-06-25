e1d734fbcb95160efee8847e0c888f868edecab7
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-06-25 19:14:04 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int n, k, dp[1001][1001];
+
+// dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
+// ex) 5C2 = 4개중 1개가 2개가 결정된 경우 + 4개중 1개가 결정된 경우
+int main(void){
+    FAST;
+
+    cin >> n >> k;
+
+    for(int i = 0; i <= n; i++){
+        dp[i][1] = i; // nC1 -> i
+        dp[i][0] = 1; // nC0 -> 1
+        dp[i][i] = 1; // nCn -> 1
+    } //dp table init
+
+    for(int i = 2; i <= n; i++){ // 2Cn start
+        for(int j = 1; j < i; j++){
+            dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            dp[i][j] %= 10007;
+        }
+    }
+
+    cout << dp[n][k];
+
+    return 0;
+}
