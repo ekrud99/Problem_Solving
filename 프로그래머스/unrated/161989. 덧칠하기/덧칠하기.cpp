b1b0440f9a1b1c3679c45f10967c16f129bbfcb7
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-06-25 20:48:55 +0900

    [unrated] Title: 덧칠하기, Time: 0.41 ms, Memory: 5.23 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0, pos = 0, cnt = 0, i = 0;
+    
+    vector<int> vec;
+    vec.resize(n + 1);
+    
+    rep(i, 0, section.size()){ // 칠해야 할 곳 1, 칠해진 곳 0
+        vec[section[i]] = 1;
+    }
+    
+    rep(i, 1, n + 1){
+        if(vec[i]){
+            cnt++;
+            if(i + m - 1 > n){
+                break;
+            }
+            rep(j, 0, m){
+                vec[i + j] = 0;
+            }
+        }
+    }
+    
+    return cnt;
+}
