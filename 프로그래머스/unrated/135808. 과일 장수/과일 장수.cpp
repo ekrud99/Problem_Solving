c08dce7f59ac78291e70097e32546c1a7b155d09
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-06-29 00:53:34 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+#define rep(i, a, b) for(auto i = 0; i < b; ++i)
+
+using namespace std;
+
+
+
+priority_queue<int> pq;
+struct cmp{
+    bool operator()(int a, int b){
+        return a>b;
+    }
+};
+priority_queue<int, vector<int>, cmp> q;
+int solution(int k, int m, vector<int> score) {
+    int answer = 0, sum = 0;
+    
+    rep(i, 0, score.size()){
+        pq.push(score[i]);
+    }
+    
+    
+    rep(i, 0, score.size() / m){
+        q = priority_queue<int, vector<int>, cmp>();
+        rep(j, 0, m){
+            q.push(pq.top());
+            pq.pop();
+        }
+        sum += q.top() * m;
+    }
+    
+    
+    return sum;
+}
