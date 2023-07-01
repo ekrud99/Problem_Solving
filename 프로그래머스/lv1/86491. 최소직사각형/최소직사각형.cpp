1597eed55fff1b7df8fac265f489899cb0d70144
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-07-01 23:57:53 +0900

    [level 1] Title: 최소직사각형, Time: 0.72 ms, Memory: 5.13 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
@@ -10,10 +10,8 @@ int solution(vector<vector<int>> sizes) {
     
     rep(i, 0, sizes.size()){
         rep(j, 0, 2){
-            int tmp = sizes[i][0];
             if(sizes[i][0] > sizes[i][1]){
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;    
+                swap(sizes[i][0], sizes[i][1]);
             }
             max_x = max(sizes[i][0], max_x);
             max_y = max(sizes[i][1], max_y);
