3ed83e41c16539d7960a6c8b3746e5293455569e
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-07-03 18:05:08 +0900

    [Silver I] Title: 회전 초밥, Time: 328 ms, Memory: 2388 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cc
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -9,8 +9,8 @@
 using namespace std;
 
 int n, d, k, c, mx = 0, cnt = 0;
-vector<int> vec;
-vector<int> sushis;
+vector<int> vec; // index 부터 k개를 먹었을 때 쿠폰가지 고려해서 먹을 수 있는 초밥의 최대값
+vector<int> sushis; // 초밥 배열
 
 
 int main(void){
@@ -26,17 +26,17 @@ int main(void){
     }
     
     rep(i, 0, n){
-        bool flag = true;
-        int cnt = 0;
-        vector<int> dup(3001);
+        bool flag = true; // 쿠폰에 있는 초밥이 등장하지 않았으면 true
+        int cnt = 0; // 중복을 제거한 초밥의 최대값 저장
+        vector<int> dup(3001); // k개의 초밥을 연속으로 먹을 동안 중복 여부
         rep(j, 0, k){
-            int sushi = sushis[(i + j) % n];
-            if(!dup[sushi]) cnt++;
-            dup[sushi] = 1;
-            if(sushi == c) flag = false;
+            int sushi = sushis[(i + j) % n]; // 현재 먹는 초밥
+            if(!dup[sushi]) cnt++; // 이전에 등장하지 않았다면 cnt += 1;
+            dup[sushi] = 1; // 중복 제거 위해 기록
+            if(sushi == c) flag = false; // 쿠폰에 있는 초밥이 등장했다면 false
         }
-        vec[i] = cnt;
-        if(flag) vec[i] += 1;
+        vec[i] = cnt; 
+        if(flag) vec[i] += 1; // 쿠폰에 있는 초밥 안먹었으면 + 1
     }
 
     rep(i, 0, n){
