7fb3f4f96d1bdf807fd6e7d92a1b3b501a5c74ec
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-07-06 22:48:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+typedef long long ll;
+ll n, k;
+vector<ll> cable;
+
+ll calc(ll len){
+    ll cnt = 0;
+    rep(i, 0, k){
+        cnt += (cable[i] / len);
+    }
+    return cnt;
+}
+
+ll b_search(){
+    ll lo = 1, hi = cable[k - 1];
+    ll res = 0;
+    while(lo <= hi){
+        ll mid = (lo + hi) / 2;
+        if(calc(mid) >= n){
+            res = max(res, mid);
+            lo = mid + 1;
+        } 
+        else hi = mid - 1;
+    }
+    return res;
+}
+
+int main(void){
+    FAST;
+
+    cin >> k >> n;
+    cable.resize(k);
+
+    rep(i, 0, k){
+        cin >> cable[i];
+    }
+
+    sort(cable.begin(), cable.end());
+
+    cout << b_search();
+
+    return 0;
+}
