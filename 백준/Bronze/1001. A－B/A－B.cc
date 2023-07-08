fa6100566f4fba5f7d9634987b75b815036c818d
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-07-08 23:42:20 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+int main(void){
+    FAST;
+    int a, b;
+    cin >> a >> b;
+    cout << a - b;
+    return 0;
+}
