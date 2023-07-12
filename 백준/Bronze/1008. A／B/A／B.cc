4219128e46fe46abfc69a863a9ab5e81a03be3a3
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-07-12 23:07:48 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+int main(void){
+    FAST;
+    double a, b;
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(9);
+    cout << a / b;
+    return 0;
+}
