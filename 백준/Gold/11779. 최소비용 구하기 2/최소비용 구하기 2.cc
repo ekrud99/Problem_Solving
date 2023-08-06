c0a7dd632a327cce27fda109ee0f10eff21cedbf
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-08-06 20:45:45 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 36 ms, Memory: 4756 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <queue>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+#define pb push_back
+#define INF numeric_limits<int>::max()
+
+using namespace std;
+
+int V, E, S, D;
+typedef pair<int, int> edge; // u, weight
+typedef pair<int, int> pii; // weight, u / 우선순위 큐에 사용할 pair
+vector<int> dist; // 각 node까지의 최단거리
+vector<bool> visited; 
+vector<vector<edge>> graph; // 방향 그래프를 인접 리스트 형태로
+vector<int> route;
+vector<int> path;
+priority_queue<pii, vector<pii>, greater<pii>> pq; // min heap의 형태로 선언
+
+int main(void){
+    FAST;
+
+    cin >> V >> E; // 정점 개수, 간선 개수, 시작 정점
+
+    dist.resize(V + 1);
+    visited.resize(V + 1);
+    graph.resize(V + 1);
+    route.resize(V + 1);
+
+    fill(dist.begin(), dist.end(), INF); // dist의 초기값은 INF
+    visited = {false, };
+
+    rep(i, 0, E){ // 그래프를 인접리스트로 표현
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].pb(edge(v, w));
+    }
+
+    cin >> S >> D;
+
+    pq.push(pii(0, S)); // 시작 node push, pii.first가 0이므로 가장 높은 우선순위를 가짐
+    dist[S] = 0; // 자신까지의 거리는 0
+
+    while(!pq.empty()){
+        pii cur = pq.top();
+        pq.pop();
+        int cur_node = cur.second;
+      
+        if(visited[cur_node]) continue; // 이미 방문했다면 continue
+        visited[cur_node] = true;
+
+        rep(i, 0, graph[cur_node].size()){ // 선택된 node 기준 dist 갱신
+            edge tmp = graph[cur_node][i];
+            int next_node = tmp.first;
+            int weight = tmp.second;
+
+            if(dist[next_node] > dist[cur_node] + weight){ // 기존 dist 보다 선택된 node 거쳐가는 경우가 이득인 경우 갱신
+                dist[next_node] = weight + dist[cur_node];
+                pq.push(pii(dist[next_node], next_node)); // 갱신했으면 pq에 push
+                route[next_node] = cur_node; // 갱신이 일어날 때 마다 어느 정점을 거쳤는지 기록
+            }
+        }
+    }
+
+    cout << dist[D] << endl; // 목적지 까지 가는 최소 비용
+
+    int cur_node = D; // 목적지부터 역추적
+
+    while(cur_node != S){ // 시작 node를 만날 때 까지 경로를 담아줌
+        path.pb(cur_node);
+        cur_node = route[cur_node];
+    }
+
+    path.pb(S); // 포함 안된 시작노드 push
+
+    reverse(path.begin(), path.end()); // 역추적된 경로이므로 reverse
+
+    cout << path.size() << endl; // 경로에 포함된 node의 개수 = path vectore의 size
+
+    for(int node : path) cout << node << " "; // 경로 출력
+
+    return 0;
+}
+
