900389902eb76963ffb21d91ef99c59faa3709e1
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-08-16 12:49:53 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+    FAST;
+
+    //freopen("input.txt", "r", stdin);
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+    return 0;
+}
