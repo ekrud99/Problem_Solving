3f6e6841c3d8e8927e5c151963901bd95a640574
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-08-20 23:51:18 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+typedef long long ll;
+
+int n;
+priority_queue<ll> pq;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n;
+
+    rep(i, 0, n){
+        ll num;
+        cin >> num;
+        if(num == 0){ // 0이 입력된 경우 max heap의 top을 출력
+            if(pq.empty()) cout << 0 << endl;
+            else{
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        }else{ // 자연수 입력 시 max heap에 push
+            pq.push(num);
+        }
+    }
+
+    return 0;
+}
