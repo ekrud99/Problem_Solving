0849a5e1757df58e76f8139e65ea145af65d104a
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-09-18 20:45:27 +0900

    [Silver II] Title: 지뢰, Time: 8 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.cc
+++ 백준/Silver/2232. 지뢰/지뢰.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 #define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
@@ -30,12 +29,8 @@ int main(void){
     vec.push_back(mpos(n + 1, 0));
 
     rep(i, 1, n + 1){
-        if(vec[i].second >= vec[i - 1].second && vec[i].second >= vec[i + 1].second) res.push_back(vec[i]);
+        if(vec[i].second >= vec[i - 1].second && vec[i].second >= vec[i + 1].second) cout << vec[i].first << endl;
     }
-
-    // sort(res.begin(), res.end());
-
-    for(auto i : res) cout << i.first << endl;
 
     return 0;
 }
