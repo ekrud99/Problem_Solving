5c745fb6024afdf81eaca23e2375f4bb7e62b8d6
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-09-20 15:44:18 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+int main(void){
+    FAST; long n; cin >> n; cout << n * 4; return 0;
+}
