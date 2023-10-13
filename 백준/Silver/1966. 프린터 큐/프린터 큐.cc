85c5d8a609f6670f6d660664ccc4b247c7aa0adb
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-10-13 17:54:37 +0900

    [Silver III] Title: 프린터 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -3,65 +3,47 @@
 #define endl "\n" 
 #define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define rep(i, a, b) for(auto i = a; i < b; ++i)
-#define REP(i, a, b) for(auto i = a; i <= b; ++i)
-
 #define pii pair<int, int>
 
-#define all(v) (v).begin(), (v).end()
-#define pb push_back
-#define INF numeric_limits<int>::max()
-#define PIV 1 << 20
-
 using namespace std; 
-typedef long long ll;
-typedef unsigned long long ull;
 
 int t, n, m, p;
 
 int main(){
     FAST;
-#ifndef ONLINE_JUDGE
-    clock_t start = clock();
-    freopen("input.txt", "r", stdin);
-#endif
 
     cin >> t;
 
     while(t--){
         cin >> n >> m;
-        priority_queue<pii> pq;
-        queue<pii> q;
+        priority_queue<int> pq; // 출력 우선순위 결정하는 우선순위 큐
+        queue<pii> q; // 프린터 큐
 
-        rep(i, 0, n){
+        rep(i, 0, n){ // 프린트할 문서와 문서의 위치 입력
             cin >> p;
-            pq.push(make_pair(p, i));
+            pq.push(p);
             q.push(make_pair(p, i));
         }
-        int res = 1;
+
+        int res = 1; // 프린트 되는 순서
+
         while(1){
-            if(pq.top().first == q.front().first){
-                if(q.front().second == m){
+            // 현재 출력할 문서와 우선순위가 가장 높은 문서의 우선순위가 같은 경우
+            if(pq.top() == q.front().first){ 
+                if(q.front().second == m){ // m번째 문서라면 순서 출력
                     cout << res << endl;
                     break;
                 }
-                // cout << "print out: " << q.front().first << endl;
-                pq.pop();
+                pq.pop(); // 아니면 프린트 해주고 ++res
                 q.pop();
                 ++res;
+            // 현재 출력할 문서와 우선순위 보다 높은 우선순위의 문서가 큐에 있는 경우
             }else{
-                // cout << "push back: " << q.front().second << endl;
-                auto tmp = q.front();
+                auto tmp = q.front(); // 맨 뒤로 보내줌
                 q.pop();
                 q.push(tmp);
             }
         }
-
     }
-
-#ifndef ONLINE_JUDGE
-    cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
-    
-#endif
-
     return 0;
 }
