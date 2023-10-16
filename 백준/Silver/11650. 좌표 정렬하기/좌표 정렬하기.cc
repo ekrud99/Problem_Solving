023ad5d7998cb5682100d6ba2818391e54b26054
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-10-16 23:43:24 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+typedef pair<int, int> pos;
+
+bool compare(pos p1, pos p2){
+    if(p1.first == p2.first) return p1.second < p2.second;
+    return p1.first < p2.first;
+}
+
+int n, x, y;
+vector<pair<int, int>> coor;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n;
+
+    while(n--){
+        cin >> x >> y;
+        coor.push_back(pos(x, y));
+    }
+
+    sort(coor.begin(), coor.end(), compare);
+
+    for(pos p : coor) cout << p.first << " " << p.second << endl;
+
+    return 0;
+}
