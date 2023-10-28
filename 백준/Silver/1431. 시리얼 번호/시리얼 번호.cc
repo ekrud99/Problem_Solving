269e46b6fc918f2d3c5c9b26876228b62272adc9
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-10-28 21:24:20 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+long long add_serial(string str){
+    long long res = 0;
+    rep(i, 0, str.length()){
+        if(str[i] >= 48 && str[i] <= 57) res += (str[i] - '0');
+    }
+    return res;
+}
+
+bool compare(string str1, string str2){
+    if(str1.length() != str2.length()) return str1.length() < str2.length();
+    long long sum1 = add_serial(str1);
+    long long sum2 = add_serial(str2);
+
+    if(sum1 != sum2) return sum1 < sum2;
+    return str1 < str2;
+}
+
+int n;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n;
+    vector<string> serials(n);
+
+    rep(i, 0, n){
+        cin >> serials[i];
+    }
+
+    sort(serials.begin(), serials.end(), compare);
+
+    for(string serial : serials) cout << serial << endl;
+
+    return 0;
+}
