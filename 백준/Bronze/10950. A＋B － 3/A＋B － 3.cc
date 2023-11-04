f95ef70033901d2ee2b28c90b7e154dcb418b253
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-11-04 23:10:37 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int n;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n;
+
+    while(n--){
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
