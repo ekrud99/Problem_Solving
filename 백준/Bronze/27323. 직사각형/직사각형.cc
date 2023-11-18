13edf0a0a15220107586db6349a2acc7fc9fc536
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-11-18 20:56:08 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+int a, b;
+int main(void){
+    FAST;
+    cin >> a >> b;
+    cout << a * b;
+    return 0;
+}
