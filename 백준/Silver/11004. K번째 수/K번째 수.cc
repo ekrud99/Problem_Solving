44b3893c39e51d810eb60e815c23f787cec39559
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-11-21 17:38:30 +0900

    [Silver V] Title: K번째 수, Time: 1040 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int n, k;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n >> k;
+    vector<int> vec(n);
+
+    rep(i, 0, n) cin >> vec[i];
+    sort(vec.begin(), vec.end());
+    cout << vec[k - 1];
+
+    return 0;
+}
