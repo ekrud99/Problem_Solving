18ad845fa80529f5ebd68d656f433be954521791
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2023-12-07 21:06:11 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int n, x, l, r, res = 0;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n;
+    vector<int> vec(n);
+
+    rep(i, 0, n) cin >> vec[i];
+    cin >> x;
+    sort(vec.begin(), vec.end());
+
+    l = 0, r = n - 1;
+
+    while(l < r){
+        int sum = vec[l] + vec[r];
+        if(sum == x){
+            ++res;
+            ++l;
+        }else if(sum < x){
+            ++l;
+        }else{
+            --r;
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
