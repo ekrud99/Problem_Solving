a0e8e014920619c733dec2a4c01c09d09881747d
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-01-04 18:25:05 +0900

    [Silver IV] Title: Maximum Word Frequency, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/9612. Maximum Word Frequency/Maximum Word Frequency.cc
+++ 백준/Silver/9612. Maximum Word Frequency/Maximum Word Frequency.cc
@@ -2,42 +2,26 @@
 #include <map>
 #include <vector>
 #include <algorithm>
-
 #define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define rep(i, a, b) for(auto i = a; i < b; ++i)
-#define endl "\n"
-
 using namespace std;
-
 int n, mx = -1;
 string word;
 map<string, int> mp;
 vector<string> vec;
-
 bool comp(string s1, string s2){
     return s1 > s2;
 }
-
 int main(void){
     FAST;
-
-    // freopen("input.txt", "r", stdin);
-
     cin >> n;
-
     rep(i, 0, n){
         cin >> word;
         ++mp[word];
         if(mx < mp[word]) mx = mp[word];
     }
-
-    for(auto w: mp){
-        if(w.second == mx) vec.push_back(w.first);
-    }
-
+    for(auto w: mp) if(w.second == mx) vec.push_back(w.first);
     sort(vec.begin(), vec.end(), comp);
-
     cout << vec.front() << " " << mx;
-
     return 0;
 }
