ad1997b3eeba643e86860c2f2f77713a79efe186
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-01-13 13:10:25 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+int main(void){
+    FAST;
+    int year;
+    cin >> year;
+    cout << 'A';
+    return 0;
+}
