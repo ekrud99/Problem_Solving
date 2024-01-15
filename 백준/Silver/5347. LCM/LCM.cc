2d35368446cc1ffd89b90ffd4b5baee9c65954cd
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-01-15 18:51:44 +0900

    [Silver V] Title: LCM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a, ll b){
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b){
+    return (a * b) / gcd(a, b);
+}
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    int n;
+
+    cin >> n;
+
+    while(n--){
+        ll a, b;
+        cin >> a >> b;
+        cout << lcm(a, b) << endl;
+    }
+
+    return 0;
+}
