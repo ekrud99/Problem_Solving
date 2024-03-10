eba1a38cb1d970eeef4e7b2a3c65637b5d4aee53
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-03-10 19:36:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+#define rep(i ,a, b) for(auto i = a; i < b; ++i)
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+    FAST;
+
+    int n;
+    char c;
+
+    cin >> c;
+    n = c;
+    cout << n;
+
+    return 0;
+}
