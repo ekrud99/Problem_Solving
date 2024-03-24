00bc74e4c6938015d8a9c8d23b76dfd0b1da070c
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-03-24 23:57:59 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    string str;
+    
+    cin >> str;
+    
+    if(str == "N" || str == "n") cout << "Naver D2";
+    else cout << "Naver Whale";
+
+    return 0;
+}
