e105aaf00282266ebf5e4e5482fedd3795bef16b
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-03-31 23:04:42 +0900

    [Silver V] Title: 장신구 명장 임스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25496. 장신구 명장 임스/장신구 명장 임스.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    int p, n, cnt = 0;
+
+    cin >> p >> n;
+    vector<int> vec(n);
+
+    rep(i, 0, n) cin >> vec[i];
+    sort(vec.begin(), vec.end());
+
+    p = 200 - p;
+
+    rep(i, 0, n){
+        if(p <= 0) break;
+        ++cnt;
+        p -= vec[i];
+    }
+
+    cout << cnt;
+
+    return 0;
+}
+
