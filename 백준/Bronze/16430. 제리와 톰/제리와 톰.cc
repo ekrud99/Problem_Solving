6bc5fe80d19e75af9301a555add76d0d000e7244
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-04-06 22:17:26 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+    
+    int a, b, t, j;
+    
+    cin >> a >> b;
+    cout << b - a << " " << b;
+
+    return 0;
+}
