d1fd9e24fe6fadc2f2b96549c1fa2bd4d660bd04
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-04-29 19:02:38 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define endl "\n"
+
+using namespace std;
+
+int main(void){
+    FAST;
+
+    // freopen("input.txt", "r", stdin);
+
+    int n, m, res = 0, cur = 0;
+    cin >> n >> m;
+    vector<int> vec(n);
+
+    rep(i, 0, n){
+        cin >> vec[i];
+        if(cur + vec[i] > m){
+            ++res;
+            cur = vec[i];
+            continue;
+        }else if(cur + vec[i] == m){
+            ++res;
+            cur = 0;
+            continue;
+        }
+        cur += vec[i];
+    }
+
+    if(cur > 0) ++res;
+
+    cout << res;
+
+    return 0;
+}
