0bda25f78dbbe8117877d5b61550ed6107bc7f07
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-07-03 01:56:55 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3.25 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.swift
@@ -0,0 +1,85 @@
+struct Pos: Hashable {
+    var i: Int
+    var j: Int
+}
+
+var set: Set<Pos> = []
+
+func find4(_ i: Int, _ j: Int, _ board: [[String]]) -> Bool {
+    let block = board[i][j]
+    if block == "*" { return false }
+    if board[i + 1][j] == block && board[i + 1][j + 1] == block && board[i][j + 1] == block { return true }
+    return false
+}
+
+func calcPoint(_ pos: [Pos], _ board: inout [[String]]) -> Int {
+    var res = 0
+    
+    for p in pos {
+        set.insert(Pos(i: p.i, j: p.j))
+        set.insert(Pos(i: p.i, j: p.j + 1))
+        set.insert(Pos(i: p.i + 1, j: p.j))
+        set.insert(Pos(i: p.i + 1, j: p.j + 1))
+        board[p.i][p.j] = "*"
+        board[p.i][p.j + 1] = "*"
+        board[p.i + 1][p.j] = "*"
+        board[p.i + 1][p.j + 1] = "*"
+    }
+    return set.count
+}
+
+func moveBlock(_ m: Int, _ n: Int, _ board: inout [[String]]) {
+    for i in (0..<m).reversed() {
+        for j in 0..<n {
+            if board[i][j] == "*" {
+                var idx = i - 1
+                while idx >= 0  {
+                    if board[idx][j] != "*" {
+                        board[i][j] = board[idx][j]
+                        board[idx][j] = "*"
+                        break
+                    }
+                    idx -= 1
+                }
+            }
+        }
+    }
+}
+
+func printBoard(_ m: Int, _ n: Int, _ board: [[String]]) {
+    for i in 0..<m {
+        for j in 0..<n {
+            print(board[i][j], terminator: "")
+        }
+        print()
+    }
+}
+
+func solution(_ m:Int, _ n:Int, _ board:[String]) -> Int {
+    var fBoard = [[String]](repeating: [], count: m)
+    var pos: [Pos] = []
+    var res = 0
+    
+    for i in 0..<board.count {
+        let arr = board[i].map { String($0) }
+        fBoard[i] += arr
+    }
+    
+    while true {
+        for i in 0..<m - 1 {
+            for j in 0..<n - 1 {
+                if find4(i, j, fBoard) { pos.append(Pos(i: i, j: j)) }
+            }
+        }
+        if pos.isEmpty { break }
+        
+        res += calcPoint(pos, &fBoard)
+        moveBlock(m, n, &fBoard)
+        set.removeAll()
+        pos.removeAll()
+    }
+    
+    return res
+}
+
+
