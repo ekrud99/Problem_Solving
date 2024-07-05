8ba66c6b6723e7293e738c3eb401de3fdd601e9b
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2024-07-06 01:08:12 +0900

    [level 2] Title: 문자열 압축, Time: 0.07 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+func compress(_ str: String, _ unit: Int) -> Int {
+    var compStr = str
+    var compList: [String] = []
+    var result = ""
+    var idx = 0
+    let lc = str.count
+    
+    while true {
+        if compStr == "" { break }
+        if compStr.count < unit {
+            compList.append(compStr)
+            break
+        }
+        var str = compStr.prefix(unit)
+        compList.append(String(str))
+        compStr = String(compStr.suffix(compStr.count - unit))
+    }
+    
+    var cnt = 1
+    var l = 0
+    var r = 1
+    
+    while r < compList.count {
+        if compList[l] == compList[r] {
+            cnt += 1
+            r += 1
+        } else {
+            if cnt > 1 { result += String(cnt) + compList[l] }
+            else { 
+                result += compList[l] 
+            }
+            l = r
+            r += 1
+            cnt = 1
+        }
+    }
+    
+    if cnt != 1 {
+        result += String(cnt) + compList[l]
+    } else {
+        result += compList[r - 1]
+    }
+    
+    
+    return result.count
+}
+
+func solution(_ s:String) -> Int {
+    let strList = s.compactMap { String($0) }
+    var mi = s.count * 2
+    
+    if s.count == 1 { return 1 }
+    
+    for i in 1..<s.count {
+        let len = compress(s, i)
+        if mi > len { mi = len }
+    }
+    
+    return mi
+}
